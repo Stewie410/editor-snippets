e2d7602e75965cffaa0f138f1cc891577f949437
Alex Paarfus <stewie410@gmail.com>
2025-11-11 20:13:22 -0500

    feat(logger): handle systemd logging in a saner way

--- templates/sh/systemd.sh
+++ templates/sh/systemd.sh
@@ -1,18 +1,26 @@
 #!/usr/bin/env bash
 
 # level, message [...]
+# level, <stdin
 log() {
+    set -- "${1}" "${2:-$(</dev/stdin)}" "${@:3}"
+    local level
     case "${1,,}" in
-        "0" | "emerg" )     printf '<0>%s\n' "${@:2}" >&4;;
-        "1" | "alert" )     printf '<1>%s\n' "${@:2}" >&4;;
-        "2" | "crit" )      printf '<2>%s\n' "${@:2}" >&4;;
-        "3" | "err" )       printf '%s\n' "${@:2}" >&2;;
-        "4" | "warn" )      printf '<4>%s\n' "${@:2}" >&4;;
-        "5" | "notice" )    printf '<5>%s\n' "${@:2}" >&4;;
-        "6" | "info" )      printf '<6>%s\n' "${@:2}" >&4;;
-        "7" | "debug" )     printf '<7>%s\n' "${@:2}" >&4;;
-        * )                 printf '<5>%s\n' "${@}" >&4;;
+        emerg )     level="0";;
+        alert )     level="1";;
+        crit )      level="2";;
+        err )       level="3";;
+        warn )      level="4";;
+        notice )    level="5";;
+        info )      level="6";;
+        debug )     level="7";;
     esac
+    shift
+
+    while (( $# > 0 )); do
+        printf '<%d>%s\n' "${level}" "${1}" >&4
+        shift
+    done
 }
 
 show_help() {
